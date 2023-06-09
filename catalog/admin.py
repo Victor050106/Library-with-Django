195bc83e254aac1b23db130499a3b15b91079c73
Victor <vimatoca050106@gmail.com>
2023-06-09 00:31:24 -0500

    add [admin] add admin site

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models.Book import Book
+from .models.Author import Author
+from .models.BookInstance import Book_Instance
+from .models.Genre import Genre
+from .models.Language import Language
+
+
+admin.site.register(Book)
+admin.site.register(Author)
+admin.site.register(Book_Instance)
+admin.site.register(Genre)
+admin.site.register(Language)
