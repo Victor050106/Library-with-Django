c1b2be76b97653909387697de267c5e98f5d22fb
Victor <vimatoca050106@gmail.com>
2023-06-09 10:06:32 -0500

    add [models -> BookInstance] add comment in class

--- catalog/models/BookInstance.py
+++ catalog/models/BookInstance.py
@@ -2,6 +2,7 @@ from django.db import models
 from .Book import Book
 
 class Book_Instance(models.Model):
+    """Model representing a book Instance."""
     unique_Id = models.CharField(max_length=50)
     due_back = models.DateField(default=0)
     status = models.CharField(max_length=50)
