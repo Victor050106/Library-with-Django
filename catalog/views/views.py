a238776dcc327bf01c85ec4f072f692edfa3c24e
Victor <vimatoca050106@gmail.com>
2023-06-15 15:16:53 -0500

    feat [views ->views.py]

--- catalog/views/views.py
+++ catalog/views/views.py
@@ -1,4 +1,29 @@
 from django.shortcuts import render
 
 # Create your views here.
-from
+from ..models.Author import Author
+from ..models.Book import Book
+from ..models.BookInstance import Book_Instance
+from ..models.Genre import Genre
+from ..models.Language import Language
+
+def index(request):
+    """function to home site"""
+    authors = Author.objects.all()
+    books = Book.objects.all()
+    book_Instance = Book_Instance.objects.all()
+    genres = Genre.objects.all()
+    languages = Language.objects.all()
+    
+    return render(
+        request,
+        'catalog/index.html',
+        context=
+        {
+            "authors":authors,
+            "books":books,
+            "book_Instance": book_Instance,
+            "genres":genres,
+            "languages":languages
+        }
+    )
