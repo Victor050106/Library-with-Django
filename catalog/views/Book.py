90b1a29eeb7f2866aeb2a0c4232c0126343d68d9
Victor <vimatoca050106@gmail.com>
2023-06-15 10:35:01 -0500

    add [views -> Book] created book in views

---
+++ catalog/views/Book.py
@@ -0,0 +1,27 @@
+from django.views import generic
+
+from ..models.Book import Book
+
+class BookListView(generic.ListView):
+    model = Book
+    context_object_name = 'book_list'
+    template_name = 'catalog/book/book_list.html'
+    
+
+class BookDetailView(generic.DetailView):
+    model = Book
+    context_object_name = 'book_detail'
+    template_name = 'catalog/book/book_detail.html'
+    
+    
+class BookCreateView(generic.CreateView):
+    model = Book
+    fields = [
+        'name',
+        'author',
+        'summary',
+        'ISBN',
+        'genre',
+        'language'
+    ]
+    template_name = 'catalog/book/book_form.html'
