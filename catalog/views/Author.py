3b94bd721c28092a49ae6cfbfe452140708f5a1f
Victor <vimatoca050106@gmail.com>
2023-06-15 10:33:56 -0500

    add [views -> Author] created Author in views

---
+++ catalog/views/Author.py
@@ -0,0 +1,27 @@
+from django.views import generic
+
+from ..models.Author import Author
+
+class AuthorListview(generic.ListView):
+    model = Author
+    context_object_name = 'author_list'
+    template_name = 'catalog/author/author_list.html'
+    
+
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
+    context_object_name = 'author_detail'
+    template_name = "catalog/author/author_detail.html"
+    
+
+
+class AuthorCreateView(generic.CreateView):
+    model = Author
+    fields = [
+        'name',
+        'date_of_birth',
+        'date_of_death'
+    ]
+
+    template_name = 'catalog/author/author_form.html'
