bc03b0f64957039cd93910ca4ed8ac3f201a4e87
Victor <vimatoca050106@gmail.com>
2023-06-16 11:25:35 -0500

    fix[models -> Author] code fix and add fname and lname

--- catalog/models/Author.py
+++ catalog/models/Author.py
@@ -1,15 +1,18 @@
 from django.db import models
+from django.urls import reverse
+
 
 
 class Author(models.Model):
     """Model representing a book author."""
-    name = models.CharField(max_length=50)
+    fname = models.CharField(max_length=50)
+    lname = models.CharField(max_length=50)
     date_of_birth = models.DateField(default=0)
     date_of_death = models.DateField(default=0)
 
     
     def __str__(self):
-        return self.name
+        return self.fname + self.lname
     
     def get_absolute_url(self):
         return reverse("author_detail", args=[str(self.id)])
