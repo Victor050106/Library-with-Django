74f73a8a638ea20fe79dd526a1d172e776378435
Victor <vimatoca050106@gmail.com>
2023-06-16 13:23:16 -0500

    fix [catalog -> urls]

--- catalog/urls.py
+++ catalog/urls.py
@@ -12,31 +12,31 @@ urlpatterns = [
 ]
 
 urlpatterns += [
-    path("Author",Author.AuthorListview.as_view(), name="Author"),
-    path("Author/<int:pk>",Author.AuthorDetailView.as_view(),name="Author_detail"),
-    path("Author/create",Author.AuthorCreateView.as_view(),name="create_author")
+    path("author",Author.AuthorListview.as_view(), name="Author"),
+    path("author/<int:pk>",Author.AuthorDetailView.as_view(),name="Author_detail"),
+    path("author/create",Author.AuthorCreateView.as_view(),name="create_author")
 ]
 
 urlpatterns += [
-    path("Book",Book.BookListView.as_view(),name='Book'),
-    path("Book/<int:pk>",Book.BookDetailView.as_view(),name='book_detail'),
-    path("Book/create",Book.BookCreateView.as_view(),name= 'create_Book')
+    path("book",Book.BookListView.as_view(),name='Book'),
+    path("book/<int:pk>",Book.BookDetailView.as_view(),name='book_detail'),
+    path("book/create",Book.BookCreateView.as_view(),name= 'create_Book')
 ]
 
 urlpatterns += [
-    path("BookInstance",BookInstance.BookInstanceListView.as_view(),name='BookInstance'),
-    path("BookInstance/<int:pk>",BookInstance.BookInstanceDetailView.as_view(),name='book_instance_detail'),
-    path("BookInstance/create",BookInstance.BookInstanceCreateView.as_view(),name= 'create-book_instance')
+    path("bookinstance",BookInstance.BookInstanceListView.as_view(),name='BookInstance'),
+    path("bookinstance/<int:pk>",BookInstance.BookInstanceDetailView.as_view(),name='book_instance_detail'),
+    path("bookinstance/create",BookInstance.BookInstanceCreateView.as_view(),name= 'create_book_instance')
 ]
 
 urlpatterns += [
-    path("Genre",Genre.GenreListView.as_view(),name='Genre'),
-    path("Book/<int:pk>",Genre.GenreDetailView.as_view(),name='Genre_detail'),
-    path("Book/create",Genre.GenreCreateView.as_view(),name= 'create_Genre')
+    path("genre",Genre.GenreListView.as_view(),name='Genre'),
+    path("genre/<int:pk>",Genre.GenreDetailView.as_view(),name='Genre_detail'),
+    path("genre/create",Genre.GenreCreateView.as_view(),name= 'create_Genre')
 ]
 
 urlpatterns += [
-    path("Language",Language.LanguageListView.as_view(),name='Language'),
-    path("Book/<int:pk>",Language.LanguageDetailView.as_view(),name='Language_detail'),
-    path("Book/create",Language.LanguageCreateView.as_view(),name= 'Language_Genre')
+    path("language",Language.LanguageListView.as_view(),name='Language'),
+    path("language/<int:pk>",Language.LanguageDetailView.as_view(),name='Language_detail'),
+    path("language/create",Language.LanguageCreateView.as_view(),name= 'Language_Genre')
 ]
