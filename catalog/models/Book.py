06b2f6d9ed3e46a88f59240ebb5c23418ff1d53e
Victor <vimatoca050106@gmail.com>
2023-06-16 11:28:03 -0500

    fix[models -> Book] code fix in line 9 and 10

--- catalog/models/Book.py
+++ catalog/models/Book.py
@@ -1,19 +1,20 @@
 from django.db import models
+from django.urls import reverse
 from .Author import Author
 from .Genre import Genre
 from .Language import Language
 
 class Book(models.Model):
     """Model representing a book ."""
-    name = models.CharField(max_length=50)
-    author = models.ForeignKey(Author,null=True, on_delete=models.SET_NULL )
+    title = models.CharField(max_length=200)
+    author = models.OneToOneField(Author,null=True, on_delete=models.CASCADE )
     summary = models.CharField(max_length=50)
     ISBN = models.CharField(max_length=50)
     genre = models.ForeignKey(Genre,null=True,on_delete=models.SET_NULL)
     language = models.ForeignKey(Language,null=True,on_delete=models.SET_NULL)
     
     def __str__(self):
-        return self.name
+        return self.title
     
     def get_absolute_url(self):
         return reverse("book_detail", args=[str(self.id)])
